d5d437a3f7f13a8a4898890d0adc8414c09f6221
João Marcos <joaomarcos@frwk.com.br>
2024-03-05 17:17:09 -0300

    feat: add start timer, pause timer, set timer, increase & decrease time by 5 minutes

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,5 +1,43 @@
 import state from "./state.js";
 import { bgSound } from "./sounds.js";
+import * as timer from "./timer.js";
+import * as el from "./elements.js";
+
+export function toggleRunning() {
+  state.isRunning = document.documentElement.classList.toggle("running");
+
+  timer.countdown();
+}
+
+export function reset() {
+  state.isRunning = false;
+  state.isRunning = document.documentElement.classList.remove("running");
+
+  timer.updateDisplay();
+}
+
+export function set() {
+  el.minutes.setAttribute("contentEditable", true);
+  el.minutes.focus();
+}
+
+export function increaseTime() {
+  let totalMinutes = Number(el.minutes.textContent);
+  totalMinutes += 5;
+  totalMinutes > 60 ? (totalMinutes = 60) : totalMinutes;
+
+  state.minutes = totalMinutes;
+  timer.updateDisplay();
+}
+
+export function decreaseTime() {
+  let totalMinutes = Number(el.minutes.textContent);
+  totalMinutes -= 5;
+  totalMinutes < 0 ? (totalMinutes = 0) : totalMinutes;
+
+  state.minutes = totalMinutes;
+  timer.updateDisplay();
+}
 
 export function toggleSound(soundId) {
   state.isMuted = document
--- src/FocusTimer/elements.js
+++ src/FocusTimer/elements.js
@@ -1,2 +1,6 @@
 export const controls = document.getElementById("controls");
+
 export const soundsControls = document.getElementById("sounds");
+
+export const minutes = document.getElementById("minutes");
+export const seconds = document.getElementById("seconds");
